0b4b4be5b6da23b98b7bd187ccde0d9d2ca1d92d
TailsTek <60035122+TailsTek@users.noreply.github.com>
2021-10-05 16:21:38 +0900

    changes

---
+++ EnoughHook/GameData/ClientModule.cs
@@ -0,0 +1,13 @@
+﻿using Interoping.ProcessManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnoughHook.GameData
+{
+    public class ClientModule : Module
+    {
+    }
+}
